8bf45946e3c04941ef05b45e59e07d5fdae30eea
Bart Elison <belison@gmail.com>
2010-08-17 13:16:29 -0600

    added encrypt and decrypt

--- lib/utilio/security.rb
+++ lib/utilio/security.rb
@@ -1,4 +1,6 @@
 require 'guid'
+require 'base64'
+require 'openssl'
 
 module Utilio
   class Security
@@ -17,7 +19,36 @@ module Utilio
       def generate_guid!
         Guid.new.to_s
       end
+      
+      def encrypt string
+        c = cipher
+        c.encrypt
+        c.key = key
+
+        e = c.update(string) + c.final
+        Base64.encode64(e).chop
+      end
+
+      def decrypt string, kw = {}
+        c = cipher
+        c.decrypt
+        c.key = key
+
+        c.update(Base64.decode64("#{ string }\n")) + c.final
+
+      end  
     
     end
+    
+    private
+    
+      def cipher
+        OpenSSL::Cipher::Cipher.new("aes-256-ecb")
+      end
+
+      def key
+        @key ||= "--thisIs0urKeyF0rEncryptionF0rPersona--"[0,56]
+      end
+    
   end
 end
