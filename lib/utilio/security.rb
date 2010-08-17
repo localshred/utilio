1de0464bc38c0f5ed62156dc7164fc7cb0d35f15
Bart Elison <belison@gmail.com>
2010-08-17 14:08:53 -0600

    moved the methods around to make the private ones work

--- lib/utilio/security.rb
+++ lib/utilio/security.rb
@@ -4,44 +4,41 @@ require 'openssl'
 
 module Utilio
   class Security
-    
   
-    class << self
-      
-      def hash_string(value)
-        Security.secure_digest( value )
-      end
-
-      def secure_digest(*args)
-        Digest::SHA2.hexdigest(args.flatten.join('--'))
-      end
-
-      def generate_guid!
-        Guid.new.to_s
-      end
-      
-      def encrypt string
-        c = cipher
-        c.encrypt
-        c.key = key
-
-        e = c.update(string) + c.final
-        Base64.encode64(e).chop
-      end
-
-      def decrypt string, kw = {}
-        c = cipher
-        c.decrypt
-        c.key = key
-
-        c.update(Base64.decode64("#{ string }\n")) + c.final
-
-      end  
     
+      
+    def self.hash_string(value)
+      Security.secure_digest( value )
+    end
+
+    def self.secure_digest(*args)
+      Digest::SHA2.hexdigest(args.flatten.join('--'))
+    end
+
+    def self.generate_guid!
+      Guid.new.to_s
     end
     
-    private
+    def self.encrypt string
+      c = cipher
+      c.encrypt
+      c.key = key
+
+      e = c.update(string) + c.final
+      Base64.encode64(e).chop
+    end
+
+    def self.decrypt string, kw = {}
+      c = cipher
+      c.decrypt
+      c.key = key
+
+      c.update(Base64.decode64("#{ string }\n")) + c.final
+
+    end  
     
+    private
+
       def cipher
         OpenSSL::Cipher::Cipher.new("aes-256-ecb")
       end
@@ -49,6 +46,9 @@ module Utilio
       def key
         @key ||= "--thisIs0urKeyF0rEncryptionF0rPersona--"[0,56]
       end
+    
+    
+      
     
   end
 end
